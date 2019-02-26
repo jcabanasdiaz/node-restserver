e4db73a8ae897982a2e4cac03dbdaf2bad67e662
Jorge <jorge.cabanas-diaz@realcore.de>
2019-02-26 13:05:25 +0100

    URL convertida a variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -25,8 +25,12 @@ let urlDB;
 // if (process.env.NODE_ENV === 'dev') {
 //     urlDB = `mongodb://localhost:27017/${mongoDBSettings.nombreBD}`;
 // } else {
-urlDB = `mongodb+srv://jcabanas:${mongoDBSettings.password}@jcabanas-test-36jpx.mongodb.net/${mongoDBSettings.nombreBD}?retryWrites=true`
-    // urlDB = `mongodb://jcabanas:${mongoDBSettings.password}@jcabanas-test-shard-00-02-36jpx.mongodb.net:27017/${mongoDBSettings.nombreBD}?ssl=true&replicaSet=JCABANAS-TEST-shard-0&authSource=admin&retryWrites=true`;
-    // }
+
+urlDB = process.env.MONGO_URI;
+// urlDB = `mongodb+srv://jcabanas:Realcore2018@jcabanas-test-36jpx.mongodb.net/cafe?retryWrites=true`
+// urlDB = `mongodb+srv://jcabanas:${mongoDBSettings.password}@jcabanas-test-36jpx.mongodb.net/${mongoDBSettings.nombreBD}?retryWrites=true`
+// urlDB = `mongodb://jcabanas:${mongoDBSettings.password}@jcabanas-test-shard-00-02-36jpx.mongodb.net:27017/${mongoDBSettings.nombreBD}?ssl=true&replicaSet=JCABANAS-TEST-shard-0&authSource=admin&retryWrites=true`;
+
+// }
 
 process.env.URLDB = urlDB;
